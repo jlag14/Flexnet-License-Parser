0bf30feda677e68ca0dd3a2b8fc5f5dbfd940f21
jlag14 <jlag1@umbc.edu>
2017-04-29 16:10:40 -0400

    Added basic comments to License.cs

--- LicenseCleanerJ/License.cs
+++ LicenseCleanerJ/License.cs
@@ -5,17 +5,21 @@ using System.Text;
 
 namespace LicenseCleaner
 {
+     // Represents a single software "license." A license has a name and a feature code.
     public class License
-    {
+    {   
+        // name and feature code variables
         private String featureCode = "";
         private String licenseName = "";
 
+        // simple constructor; only way to set code and name
         public License(String code, String name)
         {
             featureCode = code;
             licenseName = name;
         }
 
+        // accessors below; mutators not needed since we shouldn't be changing any data after parsing
         public String FeatureCode
         {
             get
